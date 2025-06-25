1b87fe87ef6de3d89c4c6f0ba23f57be33bb25cd
cms <cms@DS-D89.ka.raricy.com>
2025-06-25 10:55:53 +0800

    解决了忘记导入tool_bp和sitemap_bp的问题

--- app/web/__init__.py
+++ app/web/__init__.py
@@ -4,7 +4,10 @@ def register_blueprints(app: Flask):
     from .main import home_bp
     from .story import story_bp
     from .game import game_bp
-
+    from .tool import tool_bp
+    from .sitemap import sitemap_bp
     app.register_blueprint(home_bp, url_prefix='/')
     app.register_blueprint(story_bp, url_prefix='/story')
     app.register_blueprint(game_bp, url_prefix='/game')
+    app.register_blueprint(tool_bp, url_prefix='/tool')
+    app.register_blueprint(sitemap_bp, url_prefix='/sitemap')
