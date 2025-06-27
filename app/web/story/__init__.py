f16a7700cb5c871efd30ebf8bc9035896446343a
Raricy <raricycms@gmail.com>
2025-06-27 09:39:57 +0800

    story_update

--- app/web/story/__init__.py
+++ app/web/story/__init__.py
@@ -1,7 +1,41 @@
-from flask import Blueprint, render_template
-
+from flask import Blueprint, render_template, current_app, abort
+import os
+import markdown
+import json
 story_bp = Blueprint('story', __name__)
 
 @story_bp.route('/')
 def menu():
     return render_template('story/menu.html')
+
+@story_bp.route('/others')
+def others():
+    return render_template('story/others.html')
+
+@story_bp.route("/read/<story_id>")
+def story_detail(story_id):
+    # 拼出 Markdown 路径
+    md_path = os.path.join(current_app.instance_path, "stories", f"{story_id}", "story.md")
+    info_path = os.path.join(current_app.instance_path, "stories", f"{story_id}", "info.json")
+    # 判断文件是否存在
+    if not os.path.isfile(md_path):
+        abort(404)
+    if not os.path.isfile(info_path):
+        abort(404)
+    with open(info_path, "r", encoding="utf-8") as f:
+        info = json.load(f)
+    story_title = info["title"]
+    story_author = info["author"]
+    story_genre = info["genre"]
+    story_status = info["status"]
+
+    # 读取 Markdown 内容
+    with open(md_path, "r", encoding="utf-8") as f:
+        md_content = f.read()
+
+    # 转换为 HTML
+    html_content = markdown.markdown(md_content, extensions=["extra", "codehilite", "tables", "toc"])
+
+    # 渲染模板
+    return render_template("story/story_base.html", story_title=story_title, story_author=story_author, story_genre=story_genre, story_status=story_status, story_content=html_content)
+
