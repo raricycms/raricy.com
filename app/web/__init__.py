3f73f3b0d42cd57cf99907f7342a27fb87cd74d0
Raricy <raricycms@gmail.com>
2025-06-29 17:47:46 +0800

    404_page

--- app/web/__init__.py
+++ app/web/__init__.py
@@ -6,8 +6,10 @@ def register_blueprints(app: Flask):
     from .game import game_bp
     from .tool import tool_bp
     from .sitemap import sitemap_bp
+    from .error import error_bp
     app.register_blueprint(home_bp, url_prefix='/')
     app.register_blueprint(story_bp, url_prefix='/story')
     app.register_blueprint(game_bp, url_prefix='/game')
     app.register_blueprint(tool_bp, url_prefix='/tool')
     app.register_blueprint(sitemap_bp, url_prefix='/sitemap')
+    app.register_blueprint(error_bp, url_prefix='/error')
---
+++ app/web/error/__init__.py
@@ -0,0 +1,7 @@
+from flask import Blueprint, render_template
+
+error_bp = Blueprint('error', __name__)
+
+@error_bp.app_errorhandler(404)
+def page_not_found(e):
+    return render_template('errorhandlers/404.html'), 404
