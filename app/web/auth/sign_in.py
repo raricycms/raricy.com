0aca2e5e62aa84cf04f257fa812f36fbd1d69eac
unknown <raricycms@gmail.com>
2025-08-10 22:42:44 +0800

    login后重定向

--- app/web/auth/sign_in.py
+++ app/web/auth/sign_in.py
@@ -1,8 +1,18 @@
-from flask import Blueprint, render_template, request, jsonify
+from flask import Blueprint, render_template, request, jsonify, url_for
 from app.models import User
 from flask_login import login_user, logout_user, login_required
 
 from . import auth_bp
+
+from urllib.parse import urlparse, urljoin
+from flask import request
+
+def is_safe_url(target: str) -> bool:
+    if not target:
+        return False
+    ref = urlparse(request.host_url)
+    test = urlparse(urljoin(request.host_url, target))
+    return (test.scheme in ("http", "https")) and (ref.netloc == test.netloc)
 
 @auth_bp.route('/login', methods=['GET', 'POST'])
 def login():
@@ -13,7 +23,10 @@ def login():
         user = User.query.filter_by(username=username).first()
         if user and user.check_password(password):
             login_user(user)
-            return jsonify({'code': 200, 'message': '登录成功！'}), 200
+            next_url = data.get('next') or request.args.get('next')
+            if not next_url or not is_safe_url(next_url):
+                next_url = url_for('home.index')
+            return jsonify({'code': 200, 'message': '登录成功！', 'redirect_url': next_url}), 200
         else:
             return jsonify({'code': 400, 'message': '用户名或密码错误！'}), 400
     return render_template('auth/login.html')
